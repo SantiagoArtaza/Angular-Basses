b61d19ee99c9e6f839088e5f066b97f9780b5a9f
Santiago Artaza <S.ariel.out@gmail.com>
2024-09-24 20:33:14 -0300

    Terminado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
+import { counterModule } from './counter/counter.module';
+import { HeroesModule } from './heroes/heroes.module';
+
+
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
     BrowserModule,
+    counterModule,
+    HeroesModule,
     AppRoutingModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
